2b69fc9bcfb0e7aaa6f5b36bb2cca785c5cb1a9d
BocchiTheProg <bocchitheprog@gmail.com>
2023-10-19 15:15:29 +0300

    Task completed.

--- Abbr.rb
+++ Abbr.rb
@@ -1,1 +1,9 @@
-puts "Hello Abbreviation"
+if ARGV.empty? # check input
+  puts "Sorry, no input"
+else
+  words = ARGV.join(" ") # creating one string
+  puts "Your input:#{words}"
+  res = words.split.map { |word| word[0].upcase}.join #spliting and make first letter upcase
+  puts "Abbreviation:#{res}"
+end
+puts "P.S. Use spacebars between words in input"
--- Books.rb
+++ Books.rb
@@ -1,1 +1,25 @@
+class Book # class
+  attr_accessor :title, :author, :year_pub # create get and set methods
+  # Constructor
+  def initialize(title, author, year_pub)
+    @title = title
+    @author = author
+    @year_pub = year_pub
+  end
+end
+
 puts "Hello Books"
+name = "Star Wars"
+author = "Jonathan Lukas"
+year = 1990
+some_book = Book.new(name, author, year)
+puts "Info about book"
+puts "Book title:#{some_book.title}"
+puts "Book author:#{some_book.author}"
+puts "Book year of publishing:#{some_book.year_pub}"
+puts "\nChanging book info..."
+some_book.year_pub=2001
+puts "\nInfo about book"
+puts "Book title:#{some_book.title}"
+puts "Book author:#{some_book.author}"
+puts "Book year of publishing:#{some_book.year_pub}"
